0be98ebce0989d17936c817f635f48e30c96266f
SubhamJurysoft <subham.s@jurysoft.com>
2021-07-14 18:25:38 +0530

    changes done after heroku images issue

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: "http://127.0.0.1:8000/",
+    // baseURL: "https://enthousiaste-saucisson-12296.herokuapp.com/",
     headers: {
         post: {
             Accept: 'application/json'
